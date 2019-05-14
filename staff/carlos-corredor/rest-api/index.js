ec07a05f2a0caf77b6adb410ac3926604c950318
Carlos7979 <carloscorredorwg@gmail.com>
2019-05-14 23:49:01 +0200

    add PUT and DELETE methods on last iterated Manu's rest-api

--- staff/carlos-corredor/rest-api/index.js
+++ staff/carlos-corredor/rest-api/index.js
@@ -1,95 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const package = require('./package.json')
-const bodyParser = require('body-parser')
-const logic = require('./logic')
+const routes = require('./routes')
 
-const jsonParser = bodyParser.json()
-
-const { argv: [, , port = 8080] } = process
+const { env: { PORT }, argv: [, , port = PORT || 8080], } = process
 
 const app = express()
 
-app.post('/user', jsonParser, (req, res) => {
-    const { body: { name, surname, email, password } } = req
-    
-    try {
-        logic.registerUser(name, surname, email, password)
-            .then(() => res.json({ message: 'Ok, user registered. '}))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
-})
-
-app.post('/auth', jsonParser, (req, res) => {
-    const { body: { email, password } } = req
-    
-    try {
-        logic.authenticateUser(email, password)
-            .then((token) => res.json({ token: token }))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
-})
-
-app.get('/user', (req, res) => {
-    const { headers } = req
-    const token = headers.authorization.split(' ')[1]
-    try {
-        logic.retrieveUser(token)
-            .then(data => res.json({status: 'OK', data}))
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
-})
-
-app.put('/user', jsonParser, (req, res) => {
-    const { headers, body } = req
-    const token = headers.authorization.split(' ')[1]
-    data = body
-
-    try {
-        logic.updateUser(data, token)
-            .then(response => res.json(response))
-            
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
-})
-
-app.delete('/user', jsonParser, (req, res) => {
-    const { headers, body } = req
-    const token = headers.authorization.split(' ')[1]
-    password = body.password
-
-    try {
-        logic.deleteUser(token, password)
-            .then(response => res.json(response))
-            
-            .catch(({ message }) => {
-                res.status(400).json({ error: message})
-            })
-    } catch ({ message }) {
-        res.status(400).json({ error: message})
-    }
-})
-
-
-// TODO other routes
+app.use('/api', routes)
 
 app.use(function (req, res, next) {
-    debugger
     res.redirect('/')
 })
 
